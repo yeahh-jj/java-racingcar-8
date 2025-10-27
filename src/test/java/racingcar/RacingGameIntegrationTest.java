714c66ee144a3c9e3aab918cddf09a1b699aa42e
yeahh-jj <jyll1009@gmail.com>
2025-10-27 21:20:24 +0900

    feat: create RacingGameIntegrationTest

--- src/test/java/racingcar/RacingGameIntegrationTest.java
+++ src/test/java/racingcar/RacingGameIntegrationTest.java
@@ -1,5 +1,36 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+import racingcar.domain.Game;
+import racingcar.service.Service;
+
 public class RacingGameIntegrationTest {
-    
+
+    @Test
+    void 전체_흐름_테스트() {
+        Service service = new Service();
+
+        service.startGame("pobi,woni", "3");
+        Game game = service.getGame();
+
+        assertThat(game).isNotNull();
+        assertThat(game.getCars()).hasSize(2);
+
+        while (!game.isFinished()) {
+            service.playRound();
+        }
+
+        assertThat(game.isFinished()).isTrue();
+
+        List<Car> winners = service.getWinners();
+
+        // then: 우승자가 최소 1명 존재
+        assertThat(winners).isNotEmpty();
+        assertThat(winners.get(0).getName()).isIn("pobi", "woni");
+    }
+
 }
