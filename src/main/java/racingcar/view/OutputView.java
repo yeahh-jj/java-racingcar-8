d122105beb97baacfe1beba29a088e0d606efad5
yeahh-jj <jyll1009@gmail.com>
2025-10-27 18:09:52 +0900

    feat: create OutputView

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,5 +1,22 @@
 package racingcar.view;
 
+import java.util.List;
+import racingcar.dto.CarDto;
+
 public class OutputView {
-    
+
+    public static void printResult() {
+        System.out.println("실행 결과");
+    }
+
+    public static void outputRoundResult(List<CarDto> carDtos) {
+        for (CarDto carDto : carDtos) {
+            System.out.println(carDto.getName() + " : " + carDto.getMovementSymbol());
+        }
+        System.out.println();
+    }
+
+    public static void outputFinalWinner(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(",", winners));
+    }
 }
